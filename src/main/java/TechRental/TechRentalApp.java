b1b1dfb09d8bfd4d291d3045f3ebd375e6599237
Brayden Robert <W0740867@stclaircollege.ca>
2026-03-09 09:25:22 -0400

    Add item class/constructors and getters

--- src/main/java/TechRental/TechRentalApp.java
+++ src/main/java/TechRental/TechRentalApp.java
@@ -4,8 +4,29 @@ import javafx.stage.Stage;
 
 public class TechRentalApp extends Application {
 
+    public static class Items {
+        private String name;
+        private double price;
+        private String category;
+
+        public Items(String name, double price, String category) {
+            this.name = name;
+            this.price = price;
+            this.category = category;
+        }
+
+        public String getName() { return name; }
+        public double getPrice() { return price; }
+        public String getCategory() { return category; }
+
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
 
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
