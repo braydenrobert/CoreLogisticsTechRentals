3278f70fc1e84e9e7478b074d5b6f7d69486c195
Brayden Robert <W0740867@stclaircollege.ca>
2026-03-09 10:10:18 -0400

    Added quick search function and link to tableview

--- src/main/java/TechRental/TechRentalApp.java
+++ src/main/java/TechRental/TechRentalApp.java
@@ -1,11 +1,16 @@
 package TechRental;
 import javafx.application.Application;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -43,15 +48,38 @@ public class TechRentalApp extends Application {
         );
         root.setLeft(sidetab);
 
+        HBox searchBox = new HBox(10);
+        TextField searchName = new TextField();
+        searchName.setPromptText("Search");
+        searchBox.getChildren().add(searchName);
+        root.setTop(searchBox);
+
+
         TableView<Items> tableView = new TableView<>();
         ArrayList<Items> dataItems = new ArrayList<>();
 
-        dataItems.add(new Items("NA", 0.0, "NA"));
-        dataItems.add(new Items("NA", 0.0, "NA"));
-        dataItems.add(new Items("NA", 0.0, "NA"));
+        dataItems.add(new Items("NA", 0.00, "NA"));
+        dataItems.add(new Items("NA", 0.00, "NA"));
+        dataItems.add(new Items("NA", 0.00, "NA"));
+
+        /*
+        https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/TableView.html
+         */
 
         ObservableList<Items> inventory = FXCollections.observableArrayList(dataItems);
         tableView.setItems(inventory);
+
+        TableColumn<Items, String> nameCol = new TableColumn<>("Name");
+        nameCol.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getName()));
+
+        TableColumn<Items, Double> priceCol = new TableColumn<>("Price");
+        priceCol.setCellValueFactory(p -> new SimpleDoubleProperty(p.getValue().getPrice()).asObject());
+
+        TableColumn<Items, String> categoryCol = new TableColumn<>("Category");
+        categoryCol.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getCategory()));
+
+        tableView.getColumns().addAll(nameCol, priceCol, categoryCol);
+        root.setCenter(tableView);
 
         Scene scene = new Scene (root, 500, 300);
         stage.setTitle("Tech Rental");
