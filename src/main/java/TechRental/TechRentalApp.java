b011c430a067258f30fa43253636755178d30bf1
Brayden Robert <W0740867@stclaircollege.ca>
2026-03-09 09:07:24 -0400

    JavaFX addition to build.gradle

--- src/main/java/TechRental/TechRentalApp.java
+++ src/main/java/TechRental/TechRentalApp.java
@@ -1,5 +1,11 @@
 package TechRental;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class TechRentalApp {
+public class TechRentalApp extends Application {
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
