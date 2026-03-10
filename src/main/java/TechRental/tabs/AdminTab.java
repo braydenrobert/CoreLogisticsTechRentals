f9426aa1d200ae5019d26bb295f1f025408978f7
Brayden Robert <W0740867@stclaircollege.ca>
2026-03-10 02:36:01 -0400

    #3 Start of admin button

--- src/main/java/TechRental/tabs/AdminTab.java
+++ src/main/java/TechRental/tabs/AdminTab.java
@@ -1,8 +1,12 @@
 package TechRental.tabs;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 
 public class AdminTab extends Tab {
     public AdminTab() {
+        Button addItemButton = new Button("Add Item");
+
+        Button deleteItemButton = new Button("Delete Item");
     }
 }
