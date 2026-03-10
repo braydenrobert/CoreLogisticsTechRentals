eb7de97506e4f524619e0df6758217afc67cf0d1
Brayden Robert <W0740867@stclaircollege.ca>
2026-03-10 00:22:11 -0400

    #1 Moving tableview/arraylist into the rent tab(reorganizing now template was created)

--- src/main/java/TechRental/tabs/HomeTab.java
+++ src/main/java/TechRental/tabs/HomeTab.java
@@ -5,9 +5,5 @@ import javafx.scene.control.Tab;
 public class HomeTab extends Tab {
     public HomeTab() {
         this.setText("Home");
-
-        /*
-        Tabs not displaying yet - adding comment to make an issue ticket
-         */
     }
 }
--- src/main/java/TechRental/tabs/RentTab.java
+++ src/main/java/TechRental/tabs/RentTab.java
@@ -5,5 +5,7 @@ import javafx.scene.control.Tab;
 public class RentTab extends Tab {
     public RentTab() {
         this.setText("Rentals");
+
+        //Swapping code to here from TechRentalApp
     }
 }
