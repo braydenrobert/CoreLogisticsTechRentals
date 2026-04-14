718a7a7ba593f39ce0bcaadfbf80266ae82f53e3
Brayden Robert <W0740867@stclaircollege.ca>
2026-04-14 01:34:44 -0400

    #7 DatabaseItem file finished
    Short easy getter/setter file

--- src/main/java/TechRental/pojo/DatabaseItem.java
+++ src/main/java/TechRental/pojo/DatabaseItem.java
@@ -5,4 +5,16 @@ package TechRental.pojo;
  */
 
 public class DatabaseItem {
+    private int id;
+
+    public DatabaseItem(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 }
