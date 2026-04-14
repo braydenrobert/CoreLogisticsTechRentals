7e1aff07550ad9c7590608aa3401ddaa4261196c
Brayden Robert <W0740867@stclaircollege.ca>
2026-04-14 00:33:02 -0400

    Const file without information
    adding information after finishing next two files

--- src/main/java/TechRental/database/Const.java
+++ src/main/java/TechRental/database/Const.java
@@ -1,4 +1,7 @@
 package TechRental.database;
 
 public class Const {
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASS = "";
 }
